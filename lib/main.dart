02b45ba01a14912d8f4da2221ed7f151a633969c
SGarry <hahaha50210@gmail.com>
2023-02-10 15:53:00 +0800

    feats: home page 簡單設計

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,7 @@ import 'package:grouping_project/service/auth_service.dart';
 import 'package:grouping_project/model/user_model.dart';
 import 'firebase_options.dart';
 
-// 測試 group card 是否成功導入
-import 'createGroupCard.dart';
-// 測試 card view
-import 'createCardView.dart';
+import 'home_page.dart';
 
 void main() async {
   await Firebase.initializeApp(
@@ -32,17 +29,10 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.light,
         primarySwatch: Colors.orange,
       ),
+      // 呼叫 home_page.dart
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  //State<MyHomePage> createState() => _MyHomePageState();
-  State<MyHomePage> createState() => _testPageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -56,20 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
             home: Wrapper(),
           );
         });
-  }
-}
-
-// 測試 group card component 的 page
-class _testPageState extends State<MyHomePage> {
-
-  String testTitle = 'Title';
-  String testDes = 'this is a test\nwith a looooooooooooooooooooooooooooong text';
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: CreateUpcoming('personal', 'Flutter tutorial', 'we will try something new', '9:00 PM, FEB 7, 2023', '11:00 PM, FEB 7, 2023')
-    );
   }
 }
 
