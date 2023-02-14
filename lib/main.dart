ee638b4a5311d4140f323a3af373646fd727584f
quan0715 <quan787887@gmail.com>
2023-02-14 10:42:18 +0800

    update const

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'page/cover.dart';
 import 'page/loginPage.dart';
 
