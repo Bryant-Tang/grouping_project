d57dde47f04b5fea303c2db85940c8960820bfdd
quan0715 <quan787887@gmail.com>
2023-02-14 16:31:14 +0800

    update

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -21,7 +21,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
         appBar: AppBar(
           elevation: 0.0,
           title: const Text('Sign in to Test'),
@@ -119,76 +118,5 @@ class _LoginPageState extends State<LoginPage> {
             )
           ],
         ));
-=======
-      body: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 130, horizontal: 30),
-          )
-      )
-    );
-    // y: Column(
-    //   children: <Widget>[
-    //     Padding(
-    //       padding:
-    //           const EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
-    //       child: Column(
-    //         children: [
-    //           TextFormField(
-    //             decoration: InputDecoration(
-    //                 labelText: 'Email',
-    //                 border: OutlineInputBorder(
-    //                     borderRadius: BorderRadius.circular(50.0),
-    //                     borderSide: BorderSide(
-    //                         width: 4,
-    //                         color: Color.fromARGB(255, 133, 168, 196)))),
-    //             validator: (value) =>
-    //                 value!.isEmpty ? 'Enter your email' : null,
-    //             onChanged: (value) {
-    //               setState(() {
-    //                 email = value;
-    //               });
-    //             },
-    //           ),
-    //           TextFormField(
-    //             decoration: InputDecoration(
-    //                 labelText: 'Password',
-    //                 border: OutlineInputBorder(
-    //                   borderRadius: BorderRadius.circular(50.0),
-    //                   borderSide: BorderSide(
-    //                     width: 4,
-    //                     color: Color.fromARGB(255, 133, 168, 196),
-    //                   ),
-    //                 )),
-    //             validator: (value) => value!.length < 6
-    //                 ? 'Enter password longer than 5'
-    //                 : null,
-    //             onChanged: (value) {
-    //               setState(() {
-    //                 password = value;
-    //               });
-    //             },
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     Container(
-    //       padding: const EdgeInsets.symmetric(horizontal: 50.0),
-    //       child: ElevatedButton(
-    //         child: Text(
-    //           "Email sign in",
-    //           style: TextStyle(color: Colors.white),
-    //         ),
-    //         onPressed: () async {
-    //           dynamic result = _authService.emailLogIn(email, password);
-    //           if (result != null) {
-    //             Navigator.pushReplacement(context,
-    //                 MaterialPageRoute(builder: (context) => new Home()));
-    //           }
-    //         },
-    //       ),
-    //     ),
-    //   ],
->>>>>>> f967758 (new)
   }
 }
