b3188dd214da6a1a4c40471cb8f8475538e560eb
quan0715 <quan787887@gmail.com>
2023-02-15 21:26:48 +0800

    update

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -1,11 +1,5 @@
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:grouping_project/pages/auth/cover.dart';
-// import 'package:grouping_project/pages/home/home_page.dart';
-// import 'package:grouping_project/service/auth_service.dart';
-
 // class LoginPage extends StatefulWidget {
 //   const LoginPage({Key? key}) : super(key: key);
 //   @override
@@ -121,6 +115,8 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
+  final headLineText = "登入 / 註冊";
+  final content = "已經辦理過 Grouping 帳號了嗎？\n連結其他帳號來取用 Grouping 的服務";
   final buttonUI = {
     "Apple": {"fileName": "apple.png", "name": "apple", "onPress": () {}},
     "Google": {"fileName": "google.png", "name": "google", "onPress": () {}},
@@ -153,119 +149,52 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              const SizedBox(
-                // head line
-                width: double.infinity,
-                child: Text(
-                  "登入 / 註冊",
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0Xff1E1E1E)),
-                ),
-              ),
-              const SizedBox(
-                // content
-                width: double.infinity,
-                child: Text(
-                  "已經辦理過 Grouping 帳號了嗎？\n連結其他帳號來取用 Grouping 的服務",
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0Xff717171)),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const SizedBox(
-                    width: double.infinity,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30.0))),
-                            label: Text("EMAIL / 電子郵件",
-                                style: TextStyle(
-                                    fontSize: 12, fontWeight: FontWeight.bold)),
-                            prefix: Icon(Icons.email),
-                            constraints: BoxConstraints(maxHeight: 45)),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        child: MaterialButton(
-                          onPressed: () {},
-                          shape: const RoundedRectangleBorder(
-                              side: BorderSide(color: Colors.amber, width: 2),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20))),
-                          child: const Text(
-                            "Continue with email",
-                            style: TextStyle(
-                                color: Colors.amber,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        )),
-                  ),
-                ],
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  // mainAxisSize: MainAxisSize.max,
-                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        height: 2,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: const Color(0xff707070),
-                            width: 1,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const Expanded(
-                      flex: 3,
-                      child: Text(
-                        "OR CONNECT WITH",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Color(0xff707070),
-                          fontSize: 12,
-                          fontFamily: "Noto Sans TC",
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        height: 2,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: const Color(0xff707070),
-                            width: 1,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              HeadlineWithContent(headLineText: widget.headLineText, content: widget.content),
+              // Column(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   mainAxisSize: MainAxisSize.min,
+              //   children: <Widget>[
+              //     const SizedBox(
+              //       width: double.infinity,
+              //       child: Padding(
+              //         padding:
+              //             EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+              //         child: TextField(
+              //           decoration: InputDecoration(
+              //               border: OutlineInputBorder(
+              //                   borderRadius:
+              //                       BorderRadius.all(Radius.circular(30.0))),
+              //               label: Text("EMAIL / 電子郵件",
+              //                   style: TextStyle(
+              //                       fontSize: 12, fontWeight: FontWeight.bold)),
+              //               prefix: Icon(Icons.email),
+              //               constraints: BoxConstraints(maxHeight: 45)),
+              //         ),
+              //       ),
+              //     ),
+              //     SizedBox(
+              //       width: double.infinity,
+              //       child: Padding(
+              //           padding: const EdgeInsets.symmetric(
+              //               vertical: 10, horizontal: 15),
+              //           child: MaterialButton(
+              //             onPressed: () {},
+              //             shape: const RoundedRectangleBorder(
+              //                 side: BorderSide(color: Colors.amber, width: 2),
+              //                 borderRadius:
+              //                     BorderRadius.all(Radius.circular(20))),
+              //             child: const Text(
+              //               "Continue with email",
+              //               style: TextStyle(
+              //                   color: Colors.amber,
+              //                   fontSize: 12,
+              //                   fontWeight: FontWeight.bold),
+              //             ),
+              //           )),
+              //     ),
+              //   ],
+              // ),
+              const HintTextWithLine(),
               SizedBox(
                   width: double.infinity,
                   child: Column(children: widget.buttonBuilder()))
@@ -277,7 +206,7 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class AuthButton extends StatefulWidget {
+class AuthButton extends StatelessWidget {
   final String fileName;
   final String name;
   final Void Function() onPressed;
@@ -288,19 +217,11 @@ class AuthButton extends StatefulWidget {
     required this.onPressed,
   });
   @override
-  _AuthButtonState createState() => _AuthButtonState();
-}
-
-// TODO
-// pass the name / icon parameters
-// make the layout prettier
-class _AuthButtonState extends State<AuthButton> {
-  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
       child: MaterialButton(
-          onPressed: widget.onPressed,
+          onPressed: onPressed,
           color: Colors.white,
           shape: const RoundedRectangleBorder(
               side: BorderSide(color: Colors.grey, width: 2),
@@ -309,10 +230,10 @@ class _AuthButtonState extends State<AuthButton> {
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              Image.asset("assets/images/${widget.fileName}"),
+              Image.asset("assets/images/$fileName"),
               const SizedBox(width: 10),
               Text(
-                "login with ${widget.name} account",
+                "login with $name account",
                 style: const TextStyle(
                     color: Colors.black,
                     fontSize: 12,
@@ -320,6 +241,88 @@ class _AuthButtonState extends State<AuthButton> {
               ),
             ],
           )),
+    );
+  }
+}
+class HeadlineWithContent extends StatelessWidget {
+  final String headLineText;
+  final String content;
+  final TextStyle headLineStyle = const TextStyle(
+      fontSize: 24, fontWeight: FontWeight.bold, color: Color(0Xff1E1E1E));
+  final TextStyle contentStyle = const TextStyle(
+      fontSize: 16, fontWeight: FontWeight.bold, color: Color(0Xff1E1E1E));
+  const HeadlineWithContent(
+      {super.key, required this.headLineText, required this.content});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SizedBox(
+          // head line
+          width: double.infinity,
+          child: Text(headLineText, style: headLineStyle),
+        ),
+        SizedBox(
+          // content
+          width: double.infinity,
+          child: Text(content, style: contentStyle),
+        )
+      ],
+    );
+  }
+}
+
+class HintTextWithLine extends StatelessWidget {
+  const HintTextWithLine({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Row(
+        // mainAxisSize: MainAxisSize.max,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              height: 2,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xff707070),
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+          const Expanded(
+            flex: 3,
+            child: Text(
+              "OR CONNECT WITH",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xff707070),
+                fontSize: 12,
+                fontFamily: "Noto Sans TC",
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              height: 2,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xff707070),
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
