cd8767cb355852c2abcfdea0350ca30cf04bca0e
Bryant-Tang <s97078910828@gmail.com>
2023-03-20 15:57:39 +0800

    feat: modify doc comment

--- lib/service/firestore_service.dart
+++ lib/service/firestore_service.dart
@@ -1,15 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'auth_service.dart';
 
+/// ## a `FirestoreController` instance controll all things with firestore
+/// * an instance can controll all data of one user/group
 class FirestoreController {
   late DocumentReference ownerPath;
 
+  /// * [forUser] : whether this instance is going to controll data of a user or not
+  /// * [ownerId] : the id of user/group this instance is going to controll
   FirestoreController({required bool forUser, required String ownerId}) {
     ownerPath = FirebaseFirestore.instance
         .collection(forUser ? 'user_properties' : 'group_properties')
         .doc(ownerId);
   }
 
+  /// ### add a new data to the specific path
+  /// * [processData] : the data to be added
+  /// * [collectionPath] : the path to add data, suppose to be `T<T extends DataModel>.databasePath`
+  /// * [dataId] : if not given, firestore will generate a random id
   Future<void> set(
       {required Map<String, dynamic> processData,
       required String collectionPath,
@@ -22,6 +29,11 @@ class FirestoreController {
     return;
   }
 
+  /// ### update an existed data
+  /// * [processData] : the data to be updated
+  /// * [collectionPath] : the path to update data, suppose to be `T<T extends DataModel>.databasePath`
+  /// * [dataId] : the data id
+  /// * if firestore does not exist the data of the id, throw an Error.
   Future<void> update(
       {required Map<String, dynamic> processData,
       required String collectionPath,
@@ -30,16 +42,26 @@ class FirestoreController {
     return;
   }
 
+  /// ### get an existed data
+  /// * [collectionPath] : the path to get data, suppose to be `T<T extends DataModel>.databasePath`
+  /// * [dataId] : the data id
+  /// * if firestore does not exist the data of the id, throw an Error.
   Future<DocumentSnapshot<Map<String, dynamic>>> get(
       {required String collectionPath, required String dataId}) async {
     return await ownerPath.collection(collectionPath).doc(dataId).get();
   }
 
+  /// ### get all data of specific type
+  /// * [collectionPath] : the path to get data, suppose to be `T<T extends DataModel>.databasePath`
   Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> getAll(
       {required String collectionPath}) async {
     return (await ownerPath.collection(collectionPath).get()).docs;
   }
 
+  /// ### delete an existed data
+  /// * [collectionPath] : the path to delete data, suppose to be `T<T extends DataModel>.databasePath`
+  /// * [dataId] : the data id
+  /// * if firestore does not exist the data of the id, throw an Error.
   Future<void> delete(
       {required String collectionPath, required String dataId}) async {
     await ownerPath.collection(collectionPath).doc(dataId).delete();
