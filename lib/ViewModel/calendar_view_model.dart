77b64b48937ecb8c784d47d5ced1999d1d76b6f9
Dilamissu <dilamissushen@gmail.com>
2023-04-15 16:19:12 +0800

    feats: make calendar has style of shrink card

--- lib/ViewModel/calendar_view_model.dart
+++ lib/ViewModel/calendar_view_model.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:grouping_project/model/data_controller.dart';
 import 'package:grouping_project/model/event_model.dart';
+import 'package:grouping_project/components/card_view/event_information_shrink.dart';
+import 'package:grouping_project/components/card_view/event_information_enlarge.dart';
+import 'package:grouping_project/components/card_view/card_view_template.dart';
+
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarViewModel extends ChangeNotifier {
   final DataController dataController = DataController();
   List<EventModel> events = [];
   List<EventModel> eventsByDate = [];
+  List<Widget> eventCards = [];
   DateTime? _selectedDay;
   DateTime? _focusedDay;
   CalendarFormat _calendarFormat = CalendarFormat.month;
@@ -21,12 +26,17 @@ class CalendarViewModel extends ChangeNotifier {
   DateTime? get focusedDay => _focusedDay;
   CalendarFormat get calendarFormat => _calendarFormat;
 
+  /// This is the function used for getting all event data from the database, backend only
   Future<void> getEvents() async {
     events = await dataController.downloadAll(dataTypeToGet: EventModel());
     notifyListeners();
   }
 
-  void getEventsByDate(DateTime date) async {
+  /// This is the function used for get the events by date
+  ///
+  /// - [eventsbyDate] is the list of events that will be shown on the calendar but in list of **EventModel** format
+  /// - [eventCards] is the list of events that will be shown on the calendar but in list of **Widget** format
+  Future<void> getEventsByDate(DateTime date) async {
     await getEvents();
     debugPrint('The date is: $date');
     eventsByDate = events.where((event) {
@@ -43,6 +53,25 @@ class CalendarViewModel extends ChangeNotifier {
                   DateTime(date.year, date.month, date.day, 0, 0, 0))));
     }).toList();
     debugPrint('The events are: $eventsByDate');
+    showEvents();
     notifyListeners();
+  }
+
+  /// This is the function used for expanding the event card
+  Future<void> showEvents() async {
+    eventCards = [];
+    for (int index = 0; index < eventsByDate.length; index++) {
+      debugPrint(index.toString());
+      eventCards.add(const SizedBox(
+        height: 10,
+      ));
+      EventInformationShrink shrink =
+          EventInformationShrink(eventModel: eventsByDate[index]);
+      EventInformationEnlarge enlarge =
+          EventInformationEnlarge(eventModel: eventsByDate[index]);
+
+      eventCards.add(
+          EventCardViewTemplate(detailShrink: shrink, detailEnlarge: enlarge));
+    }
   }
 }
--- lib/pages/calendar/calendar.dart
+++ lib/pages/calendar/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:grouping_project/ViewModel/calendar_view_model.dart';
+import 'package:grouping_project/pages/home/personal_dashboard/personal_event_page.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +14,6 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   late DateTime _selectedDay = DateTime.now();
   late DateTime _focusedDay = DateTime.now();
-  late List _eventToday;
-  List eventsToday = [];
   CalendarViewModel model = CalendarViewModel();
   final Map<CalendarFormat, String> _calendarFormat = {
     CalendarFormat.month: 'month'
@@ -28,6 +27,7 @@ class _CalendarPageState extends State<CalendarPage> {
     super.initState();
     WidgetsBinding.instance
         .addPostFrameCallback((_) => model.getEventsByDate(DateTime.now()));
+    showEvents();
   }
 
   @override
@@ -37,78 +37,98 @@ class _CalendarPageState extends State<CalendarPage> {
         child: Consumer<CalendarViewModel>(
           builder: (context, model, child) {
             return RefreshIndicator(
-              key: _refreshIndicatorKey,
-              onRefresh: () => model.getEvents(),
-              child: Column(
-                children: [
-                  TableCalendar(
-                    firstDay: DateTime.utc(2010, 10, 16),
-                    lastDay: DateTime.utc(2030, 3, 14),
-                    focusedDay: _focusedDay,
-                    availableCalendarFormats: _calendarFormat,
-                    daysOfWeekHeight: 20,
-                    calendarStyle: CalendarStyle(
-                      selectedDecoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primaryContainer,
-                        shape: BoxShape.circle,
+                key: _refreshIndicatorKey,
+                onRefresh: () => model.getEventsByDate(DateTime.now()),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      TableCalendar(
+                        // center Header Title,
+                        headerStyle: const HeaderStyle(
+                          formatButtonVisible: false,
+                          titleCentered: true,
+                        ),
+                        firstDay: DateTime.utc(2010, 10, 16),
+                        lastDay: DateTime.utc(2030, 3, 14),
+                        focusedDay: _focusedDay,
+                        availableCalendarFormats: _calendarFormat,
+                        daysOfWeekHeight: 20,
+                        calendarStyle: CalendarStyle(
+                          selectedDecoration: BoxDecoration(
+                            color:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            shape: BoxShape.circle,
+                          ),
+                          selectedTextStyle:
+                              Theme.of(context).textTheme.labelLarge!.copyWith(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onPrimaryContainer,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                          todayDecoration: BoxDecoration(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .secondaryContainer,
+                            shape: BoxShape.circle,
+                          ),
+                          todayTextStyle:
+                              Theme.of(context).textTheme.labelLarge!.copyWith(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onPrimaryContainer,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                        selectedDayPredicate: (day) {
+                          return isSameDay(_selectedDay, day);
+                        },
+                        onDaySelected: (selectedDay, focusedDay) {
+                          setState(() {
+                            _selectedDay = selectedDay;
+                            _focusedDay = focusedDay;
+                            model.getEventsByDate(selectedDay);
+                          });
+                        },
+                        onPageChanged: (focusedDay) {
+                          _focusedDay = focusedDay;
+                        },
                       ),
-                      selectedTextStyle:
-                          Theme.of(context).textTheme.labelLarge!.copyWith(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onPrimaryContainer,
-                                fontWeight: FontWeight.w500,
-                              ),
-                      todayDecoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.secondaryContainer,
-                        shape: BoxShape.circle,
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: model.eventCards.length,
+                          itemBuilder: (context, index) {
+                            return model.eventCards[index];
+                            // Column(
+                            //   children: [
+                            //     ListTile(
+                            //       title: Text(
+                            //         model.eventsByDate[index].title.toString(),
+                            //         style: Theme.of(context)
+                            //             .textTheme
+                            //             .labelLarge!
+                            //             .copyWith(fontWeight: FontWeight.w500),
+                            //       ),
+                            //       subtitle: Text(
+                            //         model.eventsByDate[index].introduction
+                            //             .toString(),
+                            //         style: Theme.of(context)
+                            //             .textTheme
+                            //             .labelLarge!
+                            //             .copyWith(
+                            //                 fontWeight: FontWeight.normal),
+                            //       ),
+                            //     ),
+                            //     // Text(model.eventsByDate.length.toString())
+                            //   ],
+                            // );
+                          },
+                        ),
                       ),
-                      todayTextStyle:
-                          Theme.of(context).textTheme.labelLarge!.copyWith(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onPrimaryContainer,
-                                fontWeight: FontWeight.bold,
-                              ),
-                    ),
-                    selectedDayPredicate: (day) {
-                      return isSameDay(_selectedDay, day);
-                    },
-                    onDaySelected: (selectedDay, focusedDay) {
-                      setState(() {
-                        _selectedDay = selectedDay;
-                        _focusedDay = focusedDay;
-                        model.getEventsByDate(selectedDay);
-                      });
-                    },
-                    onPageChanged: (focusedDay) {
-                      _focusedDay = focusedDay;
-                    },
+                    ],
                   ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: model.eventsByDate.length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            Card(
-                              child: ListTile(
-                                title: Text(
-                                    model.eventsByDate[index].title.toString()),
-                                subtitle: Text(model
-                                    .eventsByDate[index].introduction
-                                    .toString()),
-                              ),
-                            ),
-                            // Text(model.eventsByDate.length.toString())
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            );
+                ));
           },
         ));
   }
