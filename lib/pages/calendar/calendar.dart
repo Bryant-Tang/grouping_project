a5a6bcf0d9d38a798e67260ea1f817963cd3396e
Dilamissu <dilamissushen@gmail.com>
2023-04-15 13:10:12 +0800

    feats: temparorilly way of showing events

--- lib/pages/calendar/calendar.dart
+++ lib/pages/calendar/calendar.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:grouping_project/model/data_controller.dart';
-import 'package:grouping_project/model/event_model.dart';
+import 'package:grouping_project/ViewModel/calendar_view_model.dart';
+import 'package:grouping_project/theme/color_schemes.dart';
+import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter/material.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -11,59 +12,80 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-  late Future<void> _dataFuture;
-  List<EventModel> _events = [];
-  DateTime? _selectedDay;
-  DateTime? _focusedDay;
-  CalendarFormat _calendarFormat = CalendarFormat.month;
+  late DateTime _selectedDay;
+  late DateTime _focusedDay;
+  List eventsToday = [];
+  final Map<CalendarFormat, String> _calendarFormat = {
+    CalendarFormat.month: 'month'
+  };
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       GlobalKey<RefreshIndicatorState>();
 
   @override
   void initState() {
+    // TODO: implement initState
+    _selectedDay = DateTime.now();
+    _focusedDay = DateTime.now();
+    CalendarViewModel().getEventsByDate(DateTime.now());
     super.initState();
-    _dataFuture = refresh();
-  }
-
-  Future<void> refresh() async {
-    await DataController()
-        .downloadAll(dataTypeToGet: EventModel())
-        .then((value) {
-      setState(() {
-        _events = value;
-      });
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: TableCalendar(
-          firstDay: DateTime(2019, 1, 1),
-          lastDay: DateTime(2030, 12, 31),
-          focusedDay: DateTime.now(),
-          daysOfWeekHeight: 20,
-          selectedDayPredicate: (day) {
-            // Use `selectedDayPredicate` to determine which day is currently selected.
-            // If this returns true, then `day` will be marked as selected.
-
-            // Using `isSameDay` is recommended to disregard
-            // the time-part of compared DateTime objects.
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            if (!isSameDay(_selectedDay, selectedDay)) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              });
-            }
-          },
-          availableCalendarFormats: {CalendarFormat.month: 'month'},
-          onPageChanged: (focusedDay) {
-            // No need to call `setState()` here
-            _focusedDay = focusedDay;
+    return ChangeNotifierProvider(
+        create: (context) => CalendarViewModel(),
+        child: Consumer<CalendarViewModel>(
+          builder: (context, model, child) {
+            return RefreshIndicator(
+              key: _refreshIndicatorKey,
+              onRefresh: () => model.getEvents(),
+              child: Column(
+                children: [
+                  TableCalendar(
+                    firstDay: DateTime.utc(2010, 10, 16),
+                    lastDay: DateTime.utc(2030, 3, 14),
+                    focusedDay: _focusedDay,
+                    availableCalendarFormats: _calendarFormat,
+                    calendarStyle: CalendarStyle(
+                      selectedDecoration: BoxDecoration(
+                        color: lightColorScheme.primaryContainer,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedTextStyle: const TextStyle(color: Colors.white),
+                      todayDecoration: BoxDecoration(
+                        color: lightColorScheme.secondaryContainer,
+                        shape: BoxShape.circle,
+                      ),
+                      todayTextStyle: const TextStyle(color: Colors.white),
+                    ),
+                    selectedDayPredicate: (day) {
+                      return isSameDay(_selectedDay, day);
+                    },
+                    onDaySelected: (selectedDay, focusedDay) =>
+                        model.onDaySelected(selectedDay, focusedDay),
+                    onPageChanged: (focusedDay) {
+                      _focusedDay = focusedDay;
+                    },
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: model.eventsByDate.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            title: Text(
+                                model.eventsByDate[index].title.toString()),
+                            subtitle: Text(model
+                                .eventsByDate[index].introduction
+                                .toString()),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
           },
         ));
   }
